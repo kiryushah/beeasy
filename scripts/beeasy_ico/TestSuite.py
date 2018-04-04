f2977430b9b97f0ca847bf595c724f00634cf236
kiryushah <kiryushah@gmail.com>
2018-04-04 11:28:53 +0300

    Update path directories

--- scripts/beeasy_ico/TestSuite.py
+++ scripts/beeasy_ico/TestSuite.py
@@ -4,6 +4,7 @@ import unittest
 import sys
 import HTMLTestRunner
 from datetime import date
+import os.path
 
 IcoTestSuite = unittest.TestSuite()
 IcoTestSuite.addTest(unittest.makeSuite(Login.LogIn))
@@ -19,7 +20,7 @@ now = date.today()
 
 filename = str(now)+"-" +"Report.html"
 
-outfile = open(filename, "w")
+outfile = open(os.path.join('/home/kiryushah/PycharmProjects/crypto/reports',filename), "w")
 runner = HTMLTestRunner.HTMLTestRunner(
                 stream=outfile,
                 title='Test Report',
