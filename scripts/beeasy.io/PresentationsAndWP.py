6515356cbfa492404971765d8de8895a7c34f6cc
kiryushah <kiryushah@gmail.com>
2018-04-10 11:59:10 +0300

    A new script has been added that verifies availability presentations and white papers.

---
+++ scripts/beeasy.io/PresentationsAndWP.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+
+class ClickToAllElements(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        wait = WebDriverWait(self.driver, 40)
+
+
+    def test_ClickToAllElements(self):
+        self.driver.get("https://beeasy.io")
+        time.sleep(4)
+
+
+        main_window = self.driver.current_window_handle
+
+        #presentation_russian
+        self.driver.find_element_by_xpath("//a[@class ='page-header__button page-header__button--presentation button']").click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//div[text()='BeEasy']")
+        time.sleep(2)
+        self.driver.switch_to.window(main_window)
+
+        #whitepaper_russian
+        self.driver.find_element_by_xpath("//a[@class ='page-header__button button mod']").click()
+        time.sleep(4)
+        self.driver.find_elements_by_xpath("//section[@class='linkAnnotation']")
+        self.driver.switch_to.window(main_window)
+
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//div[@class='locales__active']").click()
+        self.driver.find_element_by_xpath("//a[@class='locales__item']").click()
+
+        #presentation_english
+        self.driver.find_element_by_xpath("//a[@class ='page-header__button page-header__button--presentation button']").click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//div[text()='BeEasy']")
+        time.sleep(2)
+        self.driver.switch_to.window(main_window)
+
+
+        #whitepaper_english
+        self.driver.find_element_by_xpath("//a[@class ='page-header__button button mod']").click()
+        time.sleep(4)
+        self.driver.find_elements_by_xpath("//section[@class='linkAnnotation']")
+        self.driver.switch_to.window(main_window)
+
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
