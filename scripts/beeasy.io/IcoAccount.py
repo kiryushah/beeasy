fc4267a4be67605c27dd06eaa575b1081f9cf8f7
kiryushah <kiryushah@gmail.com>
2018-04-10 12:03:17 +0300

    Fix class and def name

---
+++ scripts/beeasy.io/IcoAccount.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+
+class ClickToAllElements(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        wait = WebDriverWait(self.driver, 40)
+
+
+    def test_ClickToAllElements(self):
+        self.driver.get("https://beeasy.io")
+        time.sleep(4)
+
+
+        main_window = self.driver.current_window_handle
+
+
+        #login to ico
+        self.driver.find_element_by_xpath("//a[@class ='button button--user button--round']").click()
+        time.sleep(5)
+
+        self.driver.switch_to.window(main_window)
+
+        # buy_tokens_1
+        self.driver.find_element_by_xpath("//a[@class ='sale-description__button button button--white']").click()
+
+        time.sleep(2)
+        self.driver.switch_to.window(main_window)
+
+        #buy_tokens_2
+        self.driver.find_element_by_xpath("//a[@class ='promo-buttons__join button button--white']").click()
+
+        time.sleep(2)
+        self.driver.switch_to.window(main_window)
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
