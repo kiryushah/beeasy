7dd10d952d1c9846643597391cab67eb6f959e7e
kiryushah <kiryushah@gmail.com>
2018-04-12 16:24:03 +0300

    Update script

--- scripts/beeasy_ico/ChangePassword.py
+++ scripts/beeasy_ico/ChangePassword.py
@@ -3,12 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
 
 class ChangePassword(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.maximize_window()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('headless')
+        chrome_options.add_argument('no-sandbox')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         wait = WebDriverWait(self.driver, 40)
 
     def test_ChangePassword(self):
@@ -49,8 +53,13 @@ class ChangePassword(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
+
 
 
 
