8e26dc201fa6ea6f9a51baf192ecdc0372a30e2e
kiryushah <kiryushah@gmail.com>
2018-04-26 15:19:11 +0300

    Update locator

--- scripts/beeasy.io/PresentationsAndWP.py
+++ scripts/beeasy.io/PresentationsAndWP.py
@@ -14,7 +14,7 @@ class PresentationsAndWP(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
         #chrome_options.add_argument('headless')
-        #chrome_options.add_argument('no-sandbox')
+        chrome_options.add_argument('no-sandbox')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         #self.driver = webdriver.Chrome()
         self.driver.maximize_window()
