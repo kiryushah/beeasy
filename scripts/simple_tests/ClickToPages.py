5645af5fded6a71e17e944a65f8e0f0893a5b4d6
kiryushah <kiryushah@gmail.com>
2018-05-03 12:09:53 +0300

    add new script

---
+++ scripts/simple_tests/ClickToPages.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+
+class ClickToPages(unittest.TestCase):
+
+    def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('headless')
+        chrome_options.add_argument('no-sandbox')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        wait = WebDriverWait(self.driver, 40)
+
+
+    def test_ClickToPages(self):
+        self.driver.get("https://beeasy.io")
+        time.sleep(4)
+
+
+        pages=self.driver.find_elements_by_xpath("//a[@class='page-navigation__link link']")
+        for i in pages:
+            i.click()
+            time.sleep(2)
+
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
+
+
+
