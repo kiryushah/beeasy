778d3b981d446d109b31a0621fad569ad5db7d6a
kiryushah <kiryushah@gmail.com>
2018-05-10 11:33:39 +0300

    Add timers

--- scripts/simple_tests/WhitePaper.py
+++ scripts/simple_tests/WhitePaper.py
@@ -31,6 +31,7 @@ class WhitePaper(unittest.TestCase):
 
         #whitepaper_russian
         self.driver.get("https://beeasy.io/WhitePaper_3.2_Rus.pdf")
+        time.sleep(4)
         wp = self.driver.find_element_by_xpath("//div[@id='outerContainer']")
         if wp:
             print("Russian White Paper is here!")
@@ -41,6 +42,7 @@ class WhitePaper(unittest.TestCase):
 
         #whitepaper_english
         self.driver.get("https://beeasy.io/WhitePaper_3.2_Eng.pdf")
+        time.sleep(4)
         wp = self.driver.find_element_by_xpath("//div[@id='outerContainer']")
         if wp:
             print("English White Paper is here!")
